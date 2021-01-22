c176c9486b00a59a628288a1ebdbdc2c68147538
zealshah96 <zealshah96@gmail.com>
2021-01-21 19:02:05 -0500

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [Hour, setHour] = useState(0);
+
+useEffect(()=>{
+  setHour(dateDifference());
+  setInterval(() => {
+    setHour(Hour-1);
+  }, 1000);
+})
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         {Hour}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
+function dateDifference(){
+  var startDate = new Date();
+// Do your operations
+var endDate   = new Date(2021,11,31);
+var seconds = parseInt((endDate.getTime() - startDate.getTime()) / 1000);
+debugger;
+return seconds;
+}
+
 
 export default App;
